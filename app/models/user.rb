3accff3e09a73cfb49eda14af5bf9b5f9267f1a0
Keaton1992 <keatthelitterhound@gmail.com>
2020-02-20 18:55:58 -0500

    removed users table to re-implement with correct settings.

--- app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ActiveRecord::Base
-  validates :password,
-    presence: true,
-    length: {minimum: 6, maximum: 15},
-    confirmation: true
-    validates :username, presence: true, uniqueness: true
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_09_204907) do
+ActiveRecord::Schema.define(version: 2020_02_20_225052) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.text "password"
   end
 
 end
