80419de813165df8347a9d82dda87ed2f4b817b8
Keaton1992 <keatthelitterhound@gmail.com>
2020-03-02 21:00:58 -0500

    added login authentication, and user assignement for new articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,7 +14,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = User.find_by_id(session[:user_id])
     if @article.save
       flash[:success] = "Article was successfully created!"
       redirect_to article_path(@article)
