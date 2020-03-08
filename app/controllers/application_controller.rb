13fccedfcc08b933bbb09b45d6f1f3d06101db3b
Keaton1992 <keatthelitterhound@gmail.com>
2020-03-07 21:28:46 -0500

    finalized article search functionality, relocated logic to application_helper.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 
- helper_method :current_user, :logged_in?
+ helper_method :current_user, :logged_in?, :search_results
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -14,6 +14,19 @@ class ApplicationController < ActionController::Base
     if !logged_in?
       flash[:danger] = "You must be logged in to perform that action"
       redirect_to root_path
+    end
+  end
+
+  def search_results
+    unless params[:q].nil?
+      @results = []
+      Article.all.each do |article|
+        @results << article if article.title.downcase.include?(params[:q]) ||
+           article.description.downcase.include?(params[:q])
+       end
+       @results
+    else
+      Article.paginate(page: params[:page], per_page: 5)
     end
   end
 end
